8f0184d11efeba629def2442ffa2b6b632d416a8
Vinicius Santos <vncssnts31@gmail.com>
2026-04-07 18:34:06 -0300

    Update canSave condition to simplify player checks

--- src/app/(protected)/group/[code]/matches/(detail)/[id]/sort-teams/page.tsx
+++ src/app/(protected)/group/[code]/matches/(detail)/[id]/sort-teams/page.tsx
@@ -117,7 +117,7 @@ export default function SortTeamsPage({
     teams.length > 0 &&
     reserves.length === 0 &&
     teams.every(
-      (t) => t.players.length > 0 && t.players.length === playersPerTeam,
+      (t) => t.players.length > 0,
     );
 
   return (
